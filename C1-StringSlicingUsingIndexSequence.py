d718918bf80afa8dd30123fef561a7c64e11ee05
isaakKareithi <isaacsgk2@gmail.com>
2025-09-22 17:58:31 +0300

    C1-StringSlicingUsingIndexSequence.py

---
+++ C1-StringSlicingUsingIndexSequence.py
@@ -0,0 +1,15 @@
+#Program to demonstrate string slicing 
+
+#input string
+inp = 'CODINGAL'
+
+#String slicing using indexing sequence 
+print(inp[:3])
+print(inp[3:])
+print(inp[1:5:2])
+
+#Print the string in reverse order using slicing 
+print(inp[::-1])
+
+print(inp[-1:-9:-2])
+print(inp[-2:-9:-2])
