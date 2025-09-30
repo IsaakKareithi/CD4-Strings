18404c6b07bc7b7e6ebf4ac7da3be5bd913749bf
isaakKareithi <isaacsgk2@gmail.com>
2025-09-30 17:51:27 +0300

    C4-Barcode.py

---
+++ C4-Barcode.py
@@ -0,0 +1,24 @@
+def itemPrice(barcode):
+    #list to append the ASCII codes
+    li = []
+    for i in barcode:
+        n = ord(i)
+        #finding the maximum digit of the ASCII code 
+        if n//10:
+            maxi = 0
+            while n > 0 :
+                if n % 10 > maxi:
+                    maxi = n%10
+                    n = n//10
+            li.append(maxi)
+
+        else:
+            li.append(n)
+
+    #Returning the sum of list items
+    return sum(li)
+
+# driver code
+inp = input('Enter BARCODE: ')
+price = itemPrice(inp)
+print("Price of an item is: ", price)
