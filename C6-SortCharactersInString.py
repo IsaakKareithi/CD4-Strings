0e0ec013553e1957a59bb6d7c564e98a1cf96a6a
isaakKareithi <isaacsgk2@gmail.com>
2025-10-07 18:07:18 +0300

    C6-SortCharactersInString.py

---
+++ C6-SortCharactersInString.py
@@ -0,0 +1,22 @@
+#Program to sort the characters in a string in alphabetical order
+
+def sort(s):
+    li = []
+    ans = " "
+
+    for i in range(26):
+        li.append(0)
+    for i in range(len(s)):
+        ind = ord(s[i]) - ord("a")
+        li[ind] += 1
+
+    for i in range(26):
+        if li[i] >= 1:
+            for j in range(li[i]):
+                ans = ans + chr(ord('a') + i)
+
+    return ans 
+
+#Driver code
+s = input("Enter the characters: ")
+print(sort(s))
